5ef453e5ef0789b29718c06f4e70b620ce47c69b
Chizzy99 <schesworth1@aol.com>
2024-07-13 14:09:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// * Toggle Mobile Menu
+document.addEventListener('DOMContentLoaded', function () {
+	const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+	const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+
+	toggleButton.addEventListener('click', function () {
+		mobileMenu.classList.toggle('active');
+	});
+});
+
+// * Change navbar background scroll
+window.addEventListener('scroll', function () {
+	const navbar = document.querySelector('.navbar');
+
+	if (this.window.scrollY > 0) {
+		navbar.classList.add('navbar-scroll');
+	} else {
+		navbar.classList.remove('navbar-scroll');
+	}
+});
+
+document.querySelectorAll('.scroll-btn').forEach((button) => {
+	button.addEventListener('click', function (e) {
+		e.preventDefault();
+		const targetId = this.getAttribute('target');
+		const targetElement = document.getElementById(targetId);
+
+		if (targetElement) {
+			targetElement.scrollIntoView({ behavior: 'smooth' });
+		} else {
+			window.location.href = `index.html#${targetId}`;
+		}
+	});
+});
